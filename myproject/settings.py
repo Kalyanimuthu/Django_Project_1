e95bf2aa1b56e451ca529200cccfa773463369fe
Kalyanimuthu <kalyaniram@gmail.com>
2025-11-08 17:18:27 +0530

    Deploy ready

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-project-1-ozu2.onrender.com', 'web-production-dae5a.up.railway.app', 'localhost', '127.0.0.1']
 
