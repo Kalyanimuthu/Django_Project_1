6f907a271ed32eb750775f184af5c278f9d74cd8
Kalyanimuthu <kalyaniram@gmail.com>
2025-11-08 15:24:07 +0530

    Deploy ready

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['django-project-1-ozu2.onrender.com', 'localhost', '127.0.0.1']
 
