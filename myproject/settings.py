eb8ab5a2cbc10b3b09062ec60322bb3699d98ca2
Kalyanimuthu <kalyaniram@gmail.com>
2025-11-14 19:08:01 +0530

    Debug = False

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-project-1-ozu2.onrender.com', 'localhost', '127.0.0.1']
 
@@ -67,28 +67,28 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-if os.environ.get('RENDER'):
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ecommerce_db',
-            'USER': 'root',
-            'PASSWORD': 'mkalyani',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
-    }
+# if os.environ.get('RENDER'):
+#     DATABASES = {
+#         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'ecommerce_db',
+#             'USER': 'root',
+#             'PASSWORD': 'mkalyani',
+#             'HOST': 'localhost',
+#             'PORT': '3306',
+#         }
+#     }
 
 
 
